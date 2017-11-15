b931fc7aad0c2a6a15682b20d2a3615cfc0d7379
Sarah Maris <sarah@sarahmaris.com>
2017-11-15 17:19:36 -0500

    Add Store and status bar

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Platform, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { Constants } from 'expo'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import Question from './components/Question'
-import { ltgreen } from './utils/colors'
+import { ltgreen, magenta } from './utils/colors'
+
+function FlashStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Question />
-      </View>
-    );
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <FlashStatusBar backgroundColor={magenta} barStyle="light-content" />
+          <Question />
+        </View>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-});
---
+++ reducers/index.js
@@ -0,0 +1,10 @@
+import { } from '../actions'
+
+function quizzes (state = {}, action) {
+  switch (action.type) {
+    default :
+      return state
+  }
+}
+
+export default quizzes
